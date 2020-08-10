db1add6b76a0415cae879eb31bc337a938fa15a3
Jan Pierre <gamunaxx@gmail.com>
2020-08-10 12:57:24 -0500

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { ConnectionService } from './lib/connection.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-offline-online';
+  status = 'ONLINE';
+  isConnected = true;
+
+  constructor(private connectionService: ConnectionService) {
+    this.connectionService.monitor().subscribe(isConnected => {
+      // this.isConnected = isConnected;
+      console.log(isConnected);
+      this.isConnected = isConnected?.hasInternetAccess;
+
+      if (this.isConnected) {
+        this.status = "ONLINE";
+      }
+      else {
+        this.status = "OFFLINE";
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ConnectionServiceOptions, ConnectionServiceOptionsToken} from './lib/connection.service';
+import { ConnectionServiceModule } from './lib/connection-service.module';
+
+// import { ConnectionServiceModule } from 'ng-connection-service';
+
+const date = new Date().getTime();
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConnectionServiceModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ConnectionServiceOptionsToken,
+      useValue: {
+        enableHeartbeat: true,
+        // heartbeatUrl: `/favicon.ico?t=${date}`,
+        requestMethod: 'get',
+        heartbeatInterval: 3000,
+        heartbeatRetryInterval: 1000
+      } as ConnectionServiceOptions
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
